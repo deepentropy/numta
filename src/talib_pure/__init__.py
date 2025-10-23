d62b912d754fc255863fea5b08db6353288d6ae5
Begood <otremoulis@gmail.com>
2025-10-23 16:05:26 +0200

    Added Three Inside Up/Down

--- src/talib_pure/__init__.py
+++ src/talib_pure/__init__.py
@@ -6,7 +6,7 @@ from .overlap import SMA, EMA, BBANDS
 from .volume_indicators import AD, ADOSC
 from .momentum_indicators import ADX, ADXR, APO, AROON, AROONOSC, ATR, BOP, CCI
 from .statistic_functions import BETA
-from .pattern_recognition import CDL2CROWS, CDL3BLACKCROWS
+from .pattern_recognition import CDL2CROWS, CDL3BLACKCROWS, CDL3INSIDE
 
 __version__ = "0.1.0"
-__all__ = ["SMA", "EMA", "BBANDS", "AD", "ADOSC", "ADX", "ADXR", "APO", "AROON", "AROONOSC", "ATR", "BETA", "BOP", "CCI", "CDL2CROWS", "CDL3BLACKCROWS"]
+__all__ = ["SMA", "EMA", "BBANDS", "AD", "ADOSC", "ADX", "ADXR", "APO", "AROON", "AROONOSC", "ATR", "BETA", "BOP", "CCI", "CDL2CROWS", "CDL3BLACKCROWS", "CDL3INSIDE"]
